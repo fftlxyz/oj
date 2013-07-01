1f6f4c4b33423ceab805101f00cbae0ec54a0e74
fly2best <fly2best@gmail.com>
2013-07-01 11:24:50 +0800

    just test stuff

--- test/go.sh
+++ test/go.sh
@@ -1,11 +1,28 @@
 
 # mknod bridge p
 
+ret=0
+i=0
 
-python data.py | tee data| ./ac > result2
+# while [ "$ret" == "0" ]
+while [ 1 ]
+do
 
-./life_forms_3294_suffix_array < data > result1
+    python gen_data.py | tee data| ./ac_1742_2 > result2
 
-diff result1 result2
+    ../musical_theme_1743_suffix_array < data > result1
+
+    diff result1 result2
+
+    ret=$(echo $?)
+
+    i=$(($i+1))
+
+    if [ "$ret" != "0" ]; then
+        # echo $i
+        mv data data"$i"
+    fi
+
+done
 
 # rm bridge
